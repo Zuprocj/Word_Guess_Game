96afdc1fb627eb575ce1d1084b6e793331ff759a
zuprocj <jmdcorpuz@gmail.com>
2019-08-08 09:36:09 -0700

    worked on html. added bootstrap panels to index html.  worked more on javascript. tweaked global variables and started writing out game functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,42 @@
-// word list 
-var selectableWords = ["waffles","panacakes","muffin",];
+//global varaibles
 
-const maxTries = 10;            
+var wordOptions = ["waffles","panacakes","muffin","coffee","newspaper","banana","eggs"];
+var currentWordIndex= "";
+var lettersInWord = [];       
+var numBlanks = 0;                
+var blanksAndLetters = [];
+var wrongLetters = [];
 
-var guessedLetters = [];        
-var currentWordIndex;          
-var guessingWord = [];         
-var remainingGuesses = 0;       
-var gameStarted = false;        
-var hasFinished = false;           
-var wins = 0;                  
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 10;
 
-function resetGame() {
-    remainingGuesses = maxTries;
-    gameStarted = false;
+// functions
+
+function startGame() {
+    currentWordIndex = wordOptions[Math.floor(Math.random() *wordOptions.length];
+    lettersInWord = currentWordIndex.split("");
+    numBlanks = lettersInWord.length;
+
+    // reset guesses left
+    guessesLeft = 10;
+    wrongLetters = [];
+    blanksAndLetters = [];
+
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndLetters.push("_");
+    }
 }
 
-currentWordIndex = Math.floor(math.random() * (selectableWords.length));
+    document.getElementById("currentWord").innerhtml = blanksAndLetters.join(" ");
+    document.getElementById("wrongGuess").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
 
-guessedLetters = [];
-guessingWord = [];
 
-for (var i = 0; i <selectableWords[currentWordIndex].length; i++) {
-    guessingWord.push("_";)
+
+startGame();
+
+document.onkeyup = function(event) {
+    var lettersGuessed = String.fromCharCode(event.keycode).toLowerCase();
 }
